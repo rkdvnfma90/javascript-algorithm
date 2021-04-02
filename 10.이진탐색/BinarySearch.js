7122b78273d62aed339e0a45eae2d317d10c0cd6
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-04-02 13:52:51 +0900

    이잔탐색

--- 10.이진탐색/BinarySearch.js
+++ 10.이진탐색/BinarySearch.js
@@ -7,4 +7,16 @@ for (let i = 0; i <= 100000; i++) {
 const binarySearch = (data, target) => {
   let left = 0
   let right = data.length - 1
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+
+    if (data[mid] === target) return data[mid]
+    else if (data[mid] > target) right = mid - 1
+    else left = mid + 1
+  }
+
+  return null
 }
+
+binarySearch(arr, 3000)
