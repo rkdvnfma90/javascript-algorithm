c9bf1d77caacf98d79925decb12d651d01162b27
aB <rkdvnfma90@gmail.com>
2021-12-30 22:45:39 +0900

    📝 docs: 입력방법 수정

---
+++ 00.기타/입력방법/백준.js
@@ -0,0 +1,7 @@
+// 1줄 입력
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+// 여러줄 입력
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
